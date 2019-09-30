eee8ae818bf0dcf10a4bb215cfa3edfbcb0e5df2
Xiaoming Fu <xmingfu86@gmail.com>
2019-09-30 01:26:18 -0700

    styling img

--- src/pokecard.js
+++ src/pokecard.js
@@ -1,16 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Pokecard.css'
 
 const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
-    let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
-    
-class Pokecard extends Component{ 
-    render(){
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
+
+class Pokecard extends Component {
+    render() {
         let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
         return (
             <div className="Pokecard">
                 <h1 className="Pokecard-title">{this.props.name}</h1>
-                <img src={imgSrc} alt={this.props.name} />
+                <div className="Pokecard-img">
+                    <img src={imgSrc} alt={this.props.name} />
+                </div>
                 <div className="Pokecard-data">Type: {this.props.type}</div>
                 <div>EXP: {this.props.base_experience}</div>
             </div>
