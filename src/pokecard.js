c065ee5c749408160848089086aa1cd06f1a927d
Xiaoming Fu <xmingfu86@gmail.com>
2019-09-29 23:40:49 -0700

    update clear img

--- src/pokecard.js
+++ src/pokecard.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import './Pokecard.css'
 
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+    let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 class Pokecard extends Component{
+    
     render(){
         let {id, name, type, base_experience} = this.props;
-        let imgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+        let imgSrc = `${POKE_API}${padToThree(id)}.png`;
         return (
             <div className="Pokecard">
                 <h1 className="Pokecard-title">{name}</h1>
