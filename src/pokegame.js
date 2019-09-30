0afa8bd842f845292e69ad6592fec66ad25bb370
Xiaoming Fu <xmingfu86@gmail.com>
2019-09-30 00:48:45 -0700

    add total exp

--- src/pokegame.js
+++ src/pokegame.js
@@ -23,10 +23,12 @@ class Pokegame extends Component{
             let randomPokemon = hand2.splice(randomIdx, 1)[0];
             hand1.push(randomPokemon);
         }
+        let exp1 = hand1.reduce((exp, pokemon) => (exp + pokemon.base_experience),0);
+        let exp2 = hand2.reduce((exp, pokemon) => (exp + pokemon.base_experience), 0)
         return (
             <div>
-                <Pokedex pokemon={hand1}/>
-                <Pokedex pokemon={hand2}/>
+                <Pokedex pokemon={hand1} exp={exp1}/>
+                <Pokedex pokemon={hand2} exp={exp2}/>
             </div>
         )
     }
